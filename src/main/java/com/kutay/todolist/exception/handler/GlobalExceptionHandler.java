e3cabdc98010d5eeac76bc7e11b974cdb154cdfd
kyuceak <kyuceak@sabanciuniv.edu>
2025-09-26 16:14:08 +0300

    initial setup exception handling implemented

--- src/main/java/com/kutay/todolist/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/kutay/todolist/exception/handler/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ package com.kutay.todolist.exception.handler;
 import com.kutay.todolist.exception.model.ErrorResponse;
 import com.kutay.todolist.exception.todo.TodoNotFoundException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
--- src/main/java/com/kutay/todolist/exception/todo/TodoNotFoundException.java
+++ src/main/java/com/kutay/todolist/exception/todo/TodoNotFoundException.java
@@ -1,9 +1,6 @@
 package com.kutay.todolist.exception.todo;
 
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 
 public class TodoNotFoundException extends RuntimeException{
     public TodoNotFoundException(String message){
